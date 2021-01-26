f8e6fc7af8041565bfcb90e5bd71e6933e9f617e
Chafic <chafic.laro@gmail.com>
2021-01-26 06:05:09 +0000

    Playing with function expressions.

---
+++ hello.js
@@ -0,0 +1,14 @@
+const sayHello = function (name) {
+  console.log(`Hello, ${name}!`);
+}
+sayHello('Immanuel');
+sayHello('Kant');
+sayHello('Eggmon');
+
+const sayHey = () => console.log('Hey world!');
+
+sayHey();
+
+const ohHi = name => console.log(`Oh hi, ${name}...`);
+
+ohHi('Mark');
